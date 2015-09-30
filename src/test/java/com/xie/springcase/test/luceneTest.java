171297036d7acbfe41cd7c3d198cb04a99538a47
xieqinchao <xieqc073@163.com>
2015-09-30 18:05:34 +0800

    fix

--- src/test/java/com/xie/springcase/test/luceneTest.java
+++ src/test/java/com/xie/springcase/test/luceneTest.java
@@ -22,6 +22,7 @@ import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -30,70 +31,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class luceneTest {
-	Logger logger = LoggerFactory.getLogger(luceneTest.class);
-	private Directory dir;
-	
-	@BeforeClass
-	public static void initialize() {
-	}
-	
-	@AfterClass
-	public static void end() {
-	}
-	
-	public static junit.framework.Test suite() {
-		 return new JUnit4TestAdapter(luceneTest.class);
-	}
-	
-	@Test
-	public void createIndexFile() {
-		try {
-			dir = FSDirectory.open(new File("D://tmp/lucene/index"));
-			IndexWriter writer=getWriter();
-			for(int i=0; i < 10; i++) {
-				Document doc=new Document();
-				doc.add(new StringField("id", String.valueOf(i), Store.YES));
-				doc.add(new TextField("content", "test iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"+i, Store.YES));
-				writer.addDocument(doc);
-			}
-			writer.close();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void indexSearch() {
-		try {
-			Directory dir=FSDirectory.open(new File("D://tmp/lucene/index"));
-			IndexReader reader=DirectoryReader.open(dir);
-			IndexSearcher searcher=new IndexSearcher(reader);
-			Term term=new Term("content", "test");
-			TermQuery query=new TermQuery(term);
-			TopDocs topdocs=searcher.search(query, 15);
-			ScoreDoc[] scoreDocs=topdocs.scoreDocs;
-			System.out.println("查询结果总数---" + topdocs.totalHits+"最大的评分--"+topdocs.getMaxScore());
-			for(int i=0; i < scoreDocs.length; i++) {
-				int doc = scoreDocs[i].doc;
-				Document document = searcher.doc(doc);
-				System.out.println("content===="+document.get("content"));
-				System.out.println("id--" + scoreDocs[i].doc + "---scors--" + scoreDocs[i].score+"---index--"+scoreDocs[i].shardIndex);
-			}
-			reader.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/** 
-	 * 获得IndexWriter对象 
-	 * @return 
-	 * @throws Exception 
-	 */
-	public IndexWriter getWriter() throws Exception {
-		Analyzer analyzer=new StandardAnalyzer(Version.LUCENE_4_10_4);
-		IndexWriterConfig iwc=new IndexWriterConfig(Version.LUCENE_4_10_4, analyzer);
-		return new IndexWriter(dir, iwc);
-	} 
+    Logger logger = LoggerFactory.getLogger(luceneTest.class);
+
+    @BeforeClass
+    public static void initialize() {
+    }
+
+    @AfterClass
+    public static void end() {
+    }
+
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(luceneTest.class);
+    }
+
+    @Test
+    public void createIndexFile() {
+        try {
+            // Store the index in memory:
+//            Directory dir = new RAMDirectory();
+            // To store an index on disk, use this instead:
+            Directory dir = FSDirectory.open(new File("/tmp/lucene/index"));
+            Analyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_4_10_4, analyzer);
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            for (int i = 0; i < 10; i++) {
+                Document doc = new Document();
+                doc.add(new StringField("id", String.valueOf(i), Store.YES));
+                doc.add(new TextField("content", "test iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii" + i, Store.YES));
+                writer.addDocument(doc);
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void indexSearch() {
+        try {
+            Directory dir = FSDirectory.open(new File("/tmp/lucene/index"));
+            IndexReader reader = DirectoryReader.open(dir);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            Term term = new Term("content", "test");
+            TermQuery query = new TermQuery(term);
+            TopDocs topdocs = searcher.search(query, 15);
+            ScoreDoc[] scoreDocs = topdocs.scoreDocs;
+            System.out.println("查询结果总数---" + topdocs.totalHits + "最大的评分--" + topdocs.getMaxScore());
+            for (int i = 0; i < scoreDocs.length; i++) {
+                int doc = scoreDocs[i].doc;
+                Document document = searcher.doc(doc);
+                System.out.println("content====" + document.get("content"));
+                System.out.println("id--" + scoreDocs[i].doc + "---scors--" + scoreDocs[i].score + "---index--" + scoreDocs[i].shardIndex);
+            }
+            reader.close();
+            dir.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
